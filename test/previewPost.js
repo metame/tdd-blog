d3d616969b31866db067e129de5ee49892afdc07
Michael J Erwin <michaeljameserwin@gmail.com>
2015-06-21 20:35:35 +0000

    Preview tests passed & code/path cleanup

--- test/previewPost.js
+++ test/previewPost.js
@@ -13,15 +13,15 @@ describe('Preview Post', function(){
         });
     });
             
-    it('should exist', function(done){
+    it('should show preview of draft', function(done){
         request
-        .get('localhost:8080/posts/' + latestPost.permalink)
+        .get('localhost:8080/posts/preview/' + draft.permalink)
         .end(function(err, res){
             expect(err).to.not.exist;
            
             expect(res).to.exist;
             expect(res.status).to.equal(200);
-            expect(res.text).to.contain(latestPost.title);
+            expect(res.text).to.contain(draft.title);
            
             done();
         });
